760b82a6cfb10d08526b995bb3a37a8839bd59f0
editorkode <rhico.bhankzhat@yahoo.com>
2015-12-24 00:25:22 +0800

    versi 1.0.0

---
+++ js/cloudClip.js
@@ -0,0 +1,4 @@
+var ct='<br /><center>';
+ct+='<tr><td colspan="2" align="center">Copyright &copy; '+new Date().getFullYear()+' <a href="http://desainkode.blogspot.com/">desainkode</a></td></tr>';
+ct+='</center>';
+document.write(ct);
