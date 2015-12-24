be57259149b4d4f82f1cebf4c13ba24f7638e9b6
editorkode <rhico.bhankzhat@yahoo.com>
2015-12-24 11:37:21 +0800

    Update and rename cloudClip.js to scripts.js

--- js/cloudClip.js
+++
@@ -1,4 +0,0 @@
-var ct='<br /><center>';
-ct+='<tr><td colspan="2" align="center">Copyright &copy; '+new Date().getFullYear()+' <a href="http://desainkode.blogspot.com/">desainkode</a></td></tr>';
-ct+='</center>';
-document.write(ct);
