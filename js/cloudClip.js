0cbbc1d2a0c58133145bff547fa7f118d5496a74
editorkode <rhico.bhankzhat@yahoo.com>
2015-12-24 12:11:01 +0800

    Update cloudClip.js

--- js/cloudClip.js
+++ js/cloudClip.js
@@ -1,3 +1,2 @@
-var ct='<br />';
-ct+='<div align="right" style="font-size:12px;">Copyright &copy; '+new Date().getFullYear()+' <a href="http://desainkode.blogspot.com/">desainkode</a></div>';
+var ct='<div align="right" style="font-size:12px;">Copyright &copy; '+new Date().getFullYear()+' <a href="http://desainkode.blogspot.com/">desainkode</a></div>';
 document.write(ct);
